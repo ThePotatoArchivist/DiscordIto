825937e98127d449957872b5675a8deccad1726b
ThePotatoArchivist <josiahfu@gmail.com>
2025-11-04 14:56:25 -0800

    Reorder colors

--- src/main/kotlin/Colors.kt
+++ src/main/kotlin/Colors.kt
@@ -1,12 +1,12 @@
 package archives.tater.discordito
 
 val COLORS = listOf(
-//    "\u2B1C", // White
     "\uD83D\uDFE5", // Red
     "\uD83D\uDFE6", // Blue
-    "\uD83D\uDFE7", // Orange
-    "\uD83D\uDFE8", // Yellow
     "\uD83D\uDFE9", // Green
+    "\uD83D\uDFE8", // Yellow
     "\uD83D\uDFEA", // Purple
+    "\uD83D\uDFE7", // Orange
     "\uD83D\uDFEB", // Brown
+    "\u2B1C", // White
 )
